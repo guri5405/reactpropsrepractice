89fba763c396220984ced15eb69d66b8367c69c9
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-04-27 14:08:59 +0530

    Add LeftContent component for customer segmentation

---
+++ 01practicemode/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+function LeftContent() {
+  return (
+    <div className='h-full flex flex-col justify-between w-1/3 '>
+       <div className='p-6'>
+        <h3 className='mb-7  text-7xl font-bold'>Prospective <br /> <span className='text-gray-600'>Customer   </span> <br />segmentation</h3>
+        <p className='text-xl font-medium text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, non! Odio itaque placeat ipsam ut consequatur et, commodi cupiditate maxime.</p>
+       </div>
+
+      <div className='text-9xl'>
+        <i className='ri-arrow-right-up-line'></i>
+      </div>
+      
+    </div>
+  )
+}
+
+export default LeftContent
